f79df17ec89a1843df70019ae8a7312487ea6e71
AlinaSHforwork <alinashforwork@gmail.com>
2025-07-23 15:12:27 +0300

    Update server.js
    static 2.0

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'frontend', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
